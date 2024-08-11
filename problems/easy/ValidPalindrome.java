772f4c16314266176bea80a3f884f585e3ff10ed
azmabduvokhidov <azmabduvokhidov@gmail.com>
2024-08-11 19:48:17 +0500

    New problems added

--- problems/easy/ValidPalindrome.java
+++ problems/easy/ValidPalindrome.java
@@ -23,21 +23,18 @@ public class ValidPalindrome {
         left = 0; // Initialize left pointer at the start of the string
         right = s.length() - 1; // Initialize right pointer at the end of the string
 
-        // Convert the entire string to lowercase for case-insensitive comparison
-        String lowercased = s.toLowerCase();
-
         // Loop until the left pointer crosses the right pointer
         while (left <= right) {
             // If the current left character is not alphanumeric, move the left pointer to the right
-            if (!Character.isLetterOrDigit(lowercased.charAt(left))) {
+            if (!Character.isLetterOrDigit(Character.toLowerCase(s.charAt(left)))) {
                 left += 1;
                 // If the current right character is not alphanumeric, move the right pointer to the left
-            } else if (!Character.isLetterOrDigit(lowercased.charAt(right))) {
+            } else if (!Character.isLetterOrDigit(Character.toLowerCase(s.charAt(right)))) {
                 right -= 1;
                 // If both characters are alphanumeric, compare them
             } else {
                 // If the characters don't match, the string is not a palindrome
-                if (lowercased.charAt(left) != lowercased.charAt(right)) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                     return false;
                 }
 
